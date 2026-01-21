e247851ff61e6047ff21c947da3b35e52fdf0e43
Mr oats <218810664+yesawwwwwwd@users.noreply.github.com>
2026-01-21 02:17:44 -0600

    Update WhitelistedUsers.lua

--- WhitelistedUsers.lua
+++ WhitelistedUsers.lua
@@ -1,6 +1,6 @@
 return {
     ["april22471"] = true,
-    ["ThriIl3Rz"] = true,
+    ["imagineusingsolara"] = true,
     ["defnotlilabe"] = true,
     ["BobHUBtest4"] = true,
     ["alanExt2_intyV04s2z"] = true,
